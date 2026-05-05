aa24a5f6725d3d6ad569e9ec09079c7a9950c1ac
Sam <fosusamuel123@gmail.com>
2026-05-05 09:36:18 +0000

    New Fix

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -9,22 +9,29 @@ const sendEmail = async (options) => {
     // it will throw an error or fail to send unless you use ethereal email.
     
     // We create a reusable transporter object using the explicit SMTP transport
+    // const transporter = nodemailer.createTransport({
+    //   host: 'smtp.gmail.com',
+    //   port: 587,
+    //   secure: false, // Use SSL for port 465
+    //   auth: {
+    //     user: process.env.EMAIL_USER,
+    //     pass: process.env.EMAIL_PASS,
+    //   },
+    //   // Helps bypass strict timeout/TLS issues on cloud providers like Render
+    //   tls: {
+    //     rejectUnauthorized: false
+    //   },
+    //   connectionTimeout: 20000, // 20 seconds timeout
+    //   // Force IPv4. Render/Google frequently timeout or block IPv6 SMTP connections.
+    //   family: 4
+    // });
     const transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 587,
-      secure: false, // Use SSL for port 465
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-      // Helps bypass strict timeout/TLS issues on cloud providers like Render
-      tls: {
-        rejectUnauthorized: false
-      },
-      connectionTimeout: 20000, // 20 seconds timeout
-      // Force IPv4. Render/Google frequently timeout or block IPv6 SMTP connections.
-      family: 4
-    });
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
 
     // Define the email options
     const mailOptions = {
